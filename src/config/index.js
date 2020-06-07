40162af85a7d04be597b2654d5f83f7cd889857a
horitaka <takahiro.ohori@gmail.com>
2020-06-07 15:03:50 +0900

    update ogpの設定を追加

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   app: {
-    url: 'https://hogehoge',
-    title: '本の検索アプリ',
+    url: process.env.REACT_APP_WEBSITE_URL,
+    title: process.env.REACT_APP_PAGE_TITLE,
   },
   api: {
     url: process.env.REACT_APP_API_URL,
