36d107db05a8947ff18291bbce9ee8875ee7e716
TO <takahiro.ohori@gmail.com>
2020-06-12 21:25:39 +0900

    update: 不要なファイルの削除

--- src/constants/Fonts.js
+++
@@ -1,6 +0,0 @@
-export const fontXLarge = 30; // ページタイトル
-export const fontLarge = 18;  // ナビゲーションバーのタイトル、本のタイトル、検索
-export const fontMedium = 15; // 本文, リスト
-export const fontSmall = 12; // 本文, リスト
-
-export const fontFamily = "'Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', メイリオ, Meiryo, 'ＭＳ Ｐゴシック', sans-serif";
